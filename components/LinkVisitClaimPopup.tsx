467426901b41151ce0dbfcd263e2f74401138220
ss251 <ss251@uw.edu>
2025-06-11 20:56:43 +0530

    feat: update quote cast for click to claim

--- components/LinkVisitClaimPopup.tsx
+++ components/LinkVisitClaimPopup.tsx
@@ -557,7 +557,7 @@ export function LinkVisitClaimPopup({
       
       let shareUrl = `https://warpcast.com/~/compose?text=${shareText}&embeds[]=${embedUrl}`;
       
-      const quoteCastUrl = "";
+      const quoteCastUrl = "https://farcaster.xyz/qrcoindotfun/0xab1caeb3";
       if (quoteCastUrl) {
         shareUrl += `&embeds[]=${encodeURIComponent(quoteCastUrl)}`;
       }

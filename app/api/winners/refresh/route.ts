763d410a524134a32bf8543550837f612435e356
FTCHD <144691102+FTCHD@users.noreply.github.com>
2025-07-25 12:49:04 +0100

    refresh winners (#109)
    * update winners
    
    * please the compiler
    
    * please the compiler

---
+++ app/api/winners/refresh/route.ts
@@ -0,0 +1,55 @@
+import { createClient } from '@supabase/supabase-js';
+
+export async function GET() {
+  try {
+	const supabase = createClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.SUPABASE_SERVICE_ROLE_KEY!
+	);
+  
+	const { data, error } = await supabase
+		.from('winners')
+		.select('*');
+
+	if (error) throw error;
+
+	for (const winner of data) {
+		const winnerAddress = winner.winner_address;
+		
+		const dataRes = await fetch(`https://www.fc-data.xyz/address/${winnerAddress}`);
+		
+		if (!dataRes.ok) {
+			continue;
+		}
+		
+		const data = await dataRes.json();
+
+		const farcasterUsername = data.username;
+		const displayName = data.displayName;
+		const twitterUsername = data.accounts.find((account: { platform: string; username: string; }) => account.platform === 'x')?.username;
+		
+		const updatedData: Record<string, string> = {};
+		
+		if (farcasterUsername !== winner.farcaster_username) {
+			updatedData.farcaster_username = farcasterUsername;
+		}
+		if (displayName !== winner.display_name) {
+			updatedData.display_name = displayName;
+		}
+		if (twitterUsername !== winner.twitter_username) {
+			updatedData.twitter_username = twitterUsername;
+		}
+
+		if (Object.keys(updatedData).length === 0) {
+			continue;
+		}
+
+		await supabase
+			.from('winners')
+			.update(updatedData)
+			.eq('token_id', winner.token_id);
+	}
+  } catch {
+    return Response.json({ error: 'Failed to refresh winners' }, { status: 500 });
+  }
+}
